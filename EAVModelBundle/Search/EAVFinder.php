dec7e653016d7f69ac066e06ff89c8c832b9ff85
Vincent Chalnot <vchalnot@clever-age.com>
2016-07-01 20:02:52 +0200

    Fixed EAVFinder, now using the EAVQueryBuilder API Warning ! The methods do not return a pager anymore !

--- EAVModelBundle/Search/EAVFinder.php
+++ EAVModelBundle/Search/EAVFinder.php
@@ -2,181 +2,94 @@
 
 namespace CleverAge\EAVManager\EAVModelBundle\Search;
 
+use CleverAge\EAVManager\EAVModelBundle\Entity\DataRepository;
 use Doctrine\Bundle\DoctrineBundle\Registry;
-use Pagerfanta\Exception\LessThan1CurrentPageException;
-use Pagerfanta\Exception\LessThan1MaxPerPageException;
-use Pagerfanta\Exception\NotIntegerCurrentPageException;
-use Pagerfanta\Exception\NotIntegerMaxPerPageException;
-use Pagerfanta\Exception\OutOfRangeCurrentPageException;
-use Pagerfanta\Pagerfanta;
-use Sidus\EAVFilterBundle\Configuration\EAVElasticaFilterConfigurationHandler;
-use Sidus\EAVFilterBundle\DependencyInjection\Configuration;
-use Sidus\EAVModelBundle\Configuration\FamilyConfigurationHandler;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\QueryBuilder;
 use Sidus\EAVModelBundle\Model\FamilyInterface;
-use Sidus\FilterBundle\Filter\FilterFactory;
-use Symfony\Component\Config\Definition\Processor;
-use Symfony\Component\Form\Exception\AlreadySubmittedException;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
 
 /**
- * Use this service as a wrapper of the EAVFilterBundle API to find data based on attributes values
+ * Use this service as a wrapper of the EAVQueryBuilder API to find data based on attributes values
  */
 class EAVFinder
 {
     /** @var Registry */
     protected $doctrine;
 
-    /** @var FilterFactory */
-    protected $filterFactory;
-
-    /** @var FamilyConfigurationHandler */
-    protected $familyConfigurationHandler;
-
-    /** @var FormFactoryInterface */
-    protected $formFactory;
-
     /**
-     * @param Registry                   $doctrine
-     * @param FilterFactory              $filterFactory
-     * @param FamilyConfigurationHandler $familyConfigurationHandler
-     * @param FormFactoryInterface       $formFactory
+     * @param Registry $doctrine
      */
-    public function __construct(
-        Registry $doctrine,
-        FilterFactory $filterFactory,
-        FamilyConfigurationHandler $familyConfigurationHandler,
-        FormFactoryInterface $formFactory
-    ) {
+    public function __construct(Registry $doctrine)
+    {
         $this->doctrine = $doctrine;
-        $this->filterFactory = $filterFactory;
-        $this->familyConfigurationHandler = $familyConfigurationHandler;
-        $this->formFactory = $formFactory;
     }
 
     /**
-     * WARNING ! $orderBy only takes the first element into account
-     *
      * @param FamilyInterface $family
      * @param array           $filterBy
      * @param array           $orderBy
-     * @return Pagerfanta
+     *
      * @throws \UnexpectedValueException
      * @throws \LogicException
-     * @throws AlreadySubmittedException
-     * @throws InvalidOptionsException
-     * @throws \OutOfBoundsException
-     * @throws OutOfRangeCurrentPageException
-     * @throws NotIntegerMaxPerPageException
-     * @throws LessThan1CurrentPageException
-     * @throws NotIntegerCurrentPageException
-     * @throws LessThan1MaxPerPageException
+     *
+     * @return array
      */
     public function findBy(FamilyInterface $family, array $filterBy, array $orderBy = [])
     {
-        $filterConfigurationHandler = $this->buildConfigurationHandler($family, $filterBy, $orderBy);
-        $this->handleArray($filterConfigurationHandler, $filterBy, $orderBy);
+        $qb = $this->getQb($family, $filterBy, $orderBy);
 
-        return $filterConfigurationHandler->getPager();
-    }
-
-    /**
-     * @param string $code
-     * @param array  $config
-     * @return EAVElasticaFilterConfigurationHandler
-     * @throws \UnexpectedValueException
-     */
-    public function createFilterConfigurationHandler($code, array $config)
-    {
-        return new EAVElasticaFilterConfigurationHandler(
-            $code,
-            $this->doctrine,
-            $this->filterFactory,
-            $config,
-            $this->familyConfigurationHandler
-        );
+        return $qb->getQuery()->getResult();
     }
 
     /**
      * @param FamilyInterface $family
      * @param array           $filterBy
      * @param array           $orderBy
-     * @return EAVElasticaFilterConfigurationHandler
+     *
      * @throws \UnexpectedValueException
+     * @throws \LogicException
+     * @throws NonUniqueResultException
+     *
+     * @return array
      */
-    protected function buildConfigurationHandler(FamilyInterface $family, array $filterBy, array $orderBy = [])
+    public function findOneBy(FamilyInterface $family, array $filterBy, array $orderBy = [])
     {
-        $code = uniqid(); // Not important
+        $qb = $this->getQb($family, $filterBy, $orderBy);
 
-        $sortable = [];
-        foreach ($orderBy as $property => $direction) {
-            $sortable[] = $property;
-        }
-
-        $fields = [];
-        foreach ($filterBy as $property => $value) {
-            $config = null;
-            if ($family->hasAttribute($property)) {
-                $attribute = $family->getAttribute($property);
-                if ($attribute->getType()->isRelation()) {
-                    $config = [
-                        'type' => 'autocomplete_data',
-                    ];
-                }
-            }
-            $fields[$property] = $config;
-        }
-
-        // Build fake configuration on the fly
-        $config = [
-            'sidus_eav_filter' => [
-                'configurations' => [
-                    $code => [
-                        'family' => $family->getCode(),
-                        'sortable' => $sortable,
-                        'fields' => $fields,
-                    ],
-                ],
-            ],
-        ];
-
-        $configuration = new Configuration('sidus_eav_filter');
-        $processor = new Processor();
-        $config = $processor->processConfiguration($configuration, $config);
-
-        return $this->createFilterConfigurationHandler($code, $config['configurations'][$code]);
+        return $qb->getQuery()->getOneOrNullResult();
     }
 
     /**
-     * @param EAVElasticaFilterConfigurationHandler $filterConfigurationHandler
-     * @param array                                 $filterBy
-     * @param array                                 $orderBy
-     * @throws \OutOfBoundsException
-     * @throws LessThan1MaxPerPageException
-     * @throws NotIntegerCurrentPageException
-     * @throws LessThan1CurrentPageException
-     * @throws NotIntegerMaxPerPageException
-     * @throws OutOfRangeCurrentPageException
-     * @throws AlreadySubmittedException
+     * @param FamilyInterface $family
+     * @param array           $filterBy
+     * @param array           $orderBy
+     *
+     * @throws \UnexpectedValueException
      * @throws \LogicException
-     * @throws InvalidOptionsException
+     *
+     * @return QueryBuilder
      */
-    protected function handleArray(
-        EAVElasticaFilterConfigurationHandler $filterConfigurationHandler,
-        array $filterBy,
-        array $orderBy = []
-    ) {
-        /** @noinspection LoopWhichDoesNotLoopInspection */
-        foreach ($orderBy as $property => $direction) {
-            $filterConfigurationHandler->getSortConfig()
-                ->setDefaultColumn($property)
-                ->setDefaultDirection((bool) $direction)
-            ;
-            break; // @todo handle multiple sort criteria
+    public function getQb(FamilyInterface $family, array $filterBy, array $orderBy = [])
+    {
+        /** @var DataRepository $dataRepo */
+        $dataRepo = $this->doctrine->getRepository($family->getDataClass());
+        $eavQb = $dataRepo->createEAVQueryBuilder($family);
+
+        // Add order by
+        foreach ($orderBy as $attributeCode => $direction) {
+            $eavQb->addOrderBy($eavQb->a($attributeCode), $direction);
         }
-        $filterConfigurationHandler->buildForm($this->formFactory->createBuilder());
-        $filterConfigurationHandler->handleArray([
-            EAVElasticaFilterConfigurationHandler::FILTERS_FORM_NAME => $filterBy,
-        ]);
+
+        $dqlHandlers = [];
+        foreach ($filterBy as $attributeCode => $value) {
+            $attributeQb = $eavQb->a($attributeCode);
+            if (is_array($value)) {
+                $dqlHandlers[] = $attributeQb->in($value);
+            } else {
+                $dqlHandlers[] = $attributeQb->equals($value);
+            }
+        }
+
+        return $eavQb->apply($eavQb->getAnd($dqlHandlers));
     }
 }

85bfc83ec573506ff1c614f455770792371323fc
Vincent Chalnot <vchalnot@clever-age.com>
2018-11-27 15:03:44 +0100

    Adding actions to edit and read singletons

---
+++ AdminBundle/Action/EAV/EditSingletonAction.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace CleverAge\EAVManager\AdminBundle\Action\EAV;
+
+use CleverAge\EAVManager\EAVModelBundle\Entity\DataRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sidus\AdminBundle\Action\ActionInjectableInterface;
+use Sidus\AdminBundle\Admin\Action;
+use Sidus\BaseBundle\Doctrine\RepositoryFinder;
+use Sidus\EAVModelBundle\Model\FamilyInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Security("is_granted('edit', family)")
+ */
+class EditSingletonAction implements ActionInjectableInterface
+{
+    /** @var EditAction */
+    protected $editAction;
+
+    /** @var RepositoryFinder */
+    protected $repositoryFinder;
+
+    /** @var Action */
+    protected $action;
+
+    /**
+     * @param EditAction       $editAction
+     * @param RepositoryFinder $repositoryFinder
+     */
+    public function __construct(
+        EditAction $editAction,
+        RepositoryFinder $repositoryFinder
+    ) {
+        $this->editAction = $editAction;
+        $this->repositoryFinder = $repositoryFinder;
+    }
+
+    /**
+     * @param Request         $request
+     * @param FamilyInterface $family
+     *
+     * @throws \Exception
+     *
+     * @return Response
+     */
+    public function __invoke(Request $request, FamilyInterface $family): Response
+    {
+        /** @var DataRepository $repository */
+        $repository = $this->repositoryFinder->getRepository($family->getDataClass());
+        $data = $repository->getInstance($family);
+
+        return ($this->editAction)($request, $data, $family);
+    }
+
+    /**
+     * @param Action $action
+     */
+    public function setRedirectAction(Action $action): void
+    {
+        $this->editAction->setRedirectAction($action);
+    }
+
+    /**
+     * @param Action $action
+     */
+    public function setAction(Action $action): void
+    {
+        $this->action = $action;
+        $this->editAction->setAction($action);
+        $this->setRedirectAction($action);
+    }
+}
---
+++ AdminBundle/Action/EAV/ReadSingletonAction.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace CleverAge\EAVManager\AdminBundle\Action\EAV;
+
+use CleverAge\EAVManager\EAVModelBundle\Entity\DataRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sidus\AdminBundle\Action\ActionInjectableInterface;
+use Sidus\AdminBundle\Admin\Action;
+use Sidus\BaseBundle\Doctrine\RepositoryFinder;
+use Sidus\EAVModelBundle\Model\FamilyInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Security("is_granted('read', family)")
+ */
+class ReadSingletonAction implements ActionInjectableInterface
+{
+    /** @var ReadAction */
+    protected $readAction;
+
+    /** @var RepositoryFinder */
+    protected $repositoryFinder;
+
+    /** @var Action */
+    protected $action;
+
+    /**
+     * @param ReadAction       $readAction
+     * @param RepositoryFinder $repositoryFinder
+     */
+    public function __construct(
+        ReadAction $readAction,
+        RepositoryFinder $repositoryFinder
+    ) {
+        $this->readAction = $readAction;
+        $this->repositoryFinder = $repositoryFinder;
+    }
+
+    /**
+     * @param Request         $request
+     * @param FamilyInterface $family
+     *
+     * @throws \Exception
+     *
+     * @return Response
+     */
+    public function __invoke(Request $request, FamilyInterface $family): Response
+    {
+        /** @var DataRepository $repository */
+        $repository = $this->repositoryFinder->getRepository($family->getDataClass());
+        $data = $repository->getInstance($family);
+
+        return ($this->readAction)($request, $data, $family);
+    }
+
+    /**
+     * @param Action $action
+     */
+    public function setAction(Action $action): void
+    {
+        $this->action = $action;
+        $this->readAction->setAction($action);
+    }
+}
